f5c1f9be8cff2c418c828677ac17d1abdcb83813
Enzo Fernandes <enzo.fernandes0812@gmail.com>
2025-05-05 14:04:45 +0200

    ajout de la branch vers la main

--- WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
+++ WebApplication1/obj/Debug/net9.0/WebApplication1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+96b7f80fa735f1504576d7c0ad515db90076935f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fb9959636f3a76d75023d36a225bc7d2deb68e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
